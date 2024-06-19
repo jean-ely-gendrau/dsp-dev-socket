0358ed5ce2e1685f8b7fa5fe2d11ed4fc8b5aad3
Jely Gendrau <jean-ely.gendrau@laplateforme.io>
2024-06-19 20:38:05 +0200

    Ajout d'une méthode updateUser à la class UserStorage

--- storage/UserStore.js
+++ storage/UserStore.js
@@ -11,9 +11,18 @@ class UserStore {
     this.user.set(id, user);
   }
 
+  updateUser(id, user) {
+    const userSelect = this.findUser(id);
+    if (userSelect) {
+      let userUpdate = { ...user, ...userSelect }
+      this.saveUser(id, userUpdate);
+    }
+  }
+
   findAllUser() {
     return [...this.user.values()];
   }
 }
+
 
 module.exports = UserStore
